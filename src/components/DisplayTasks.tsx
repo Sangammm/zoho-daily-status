b7d2f050de0cb3c5b032f48b7793dba8ab62698d
Sangam Rajpara <sangamrajpara@gmail.com>
2020-11-26 02:29:22 +0530

    code Improvments

--- src/components/DisplayTasks.tsx
+++ src/components/DisplayTasks.tsx
@@ -1,36 +1,13 @@
 import React, { useState } from 'react'
 import { task, bug } from './StatusComp'
-
+import {
+	displayTime,
+	isInTomorrowTask,
+	isProjectManagment,
+} from '../Utils/taskFuncs'
 export interface DisplayTasksProps {
 	taskList: task[]
 	bugList: bug[]
-}
-const taskStatusTobeTested: string[] = ['open', 'in progress']
-
-export const isProjectManagment = (task: task) => {
-	return task?.tasklist?.name?.toLowerCase() === 'project management'
-}
-
-export const isInTomorrowTask = (task: task | bug) => {
-	return taskStatusTobeTested.includes(
-		task?.status?.name?.toLowerCase() || task?.status?.type?.toLowerCase()
-	)
-}
-
-export const displayTime = (task: task | bug) => {
-	const hours = Math.floor(task.total_minutes / 60)
-	const minutes = Math.floor(task.total_minutes % 60)
-	let displayTime = ''
-	if (hours !== 0) {
-		displayTime += `${hours}h`
-	}
-	if (minutes !== 0 && hours !== 0) {
-		displayTime += ':'
-	}
-	if (minutes !== 0) {
-		displayTime += `${minutes}m`
-	}
-	return displayTime
 }
 
 export const DisplayTodayTasks: React.FC<DisplayTasksProps> = ({
@@ -41,11 +18,10 @@ export const DisplayTodayTasks: React.FC<DisplayTasksProps> = ({
 	const [insights, setInsights] = useState(false)
 	const prjtMngFilter = (task: task) => {
 		if (hideProjectManagment) {
-			if (isProjectManagment(task)) {
-				return false
-			}
+			return !isProjectManagment(task)
+		} else {
+			return true
 		}
-		return true
 	}
 
 	return (
